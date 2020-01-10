5a81ae6db4435c30794a7f3305c6d4bb9daa9067
Simon Lindahl <simon.lindahl@consid.se>
2020-01-10 09:03:02 +0100

    Updates

---
+++ precache-manifest.e6430748a14a6c7a046b6df5e2673147.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112f1fedbd432705022c7bce67723648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38044dcb85462941a279",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8207e60726e64417627f",
+    "url": "/static/js/2.0260c916.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.0260c916.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38044dcb85462941a279",
+    "url": "/static/js/main.f5205f4c.chunk.js"
+  },
+  {
+    "revision": "851564d85e2e22813d5d",
+    "url": "/static/js/runtime-main.f61e8349.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
